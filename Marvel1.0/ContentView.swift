36fa190d769ed8ddbf67b118d63f5b8c0e584197
Ruy <ruymiguel.penha2003@gmail.com>
2021-05-19 08:44:55 -0400

    Initial Commit

---
+++ Marvel1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvel1.0
+//
+//  Created by user195594 on 5/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
